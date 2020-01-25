0914086defabcd4bc6dfbd18dadaaaa463a04a6a
Manoel C <manoelclemente9@outlook.com>
2020-01-25 10:55:45 -0300

    add

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
+
+
+
+
+
+
+
+
+
+function chamaApi(){
+
 fetch('https://api.chucknorris.io/jokes/random')
     .then(response => response.json())
     .then(dados => {
-        console.log(dados)
+        console.log(dados);
+        
+        document.getElementById('show').innerHTML += '<div>{dados.icon_url}</div>';
     })    
     .catch(err => console.err(err));
 
+   
+}
