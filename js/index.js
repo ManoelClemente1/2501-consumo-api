09c4a52673714a2947daa9639a7c86a05165d987
mpleandro <48145725+mpleandro@users.noreply.github.com>
2020-01-25 11:41:07 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@
 
 
 
+var ChuckImg = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg']
+var imgClick = 0
 
 
 function chamaApi(){
@@ -19,6 +21,11 @@ fetch('https://api.chucknorris.io/jokes/random')
         
         document.getElementById('lastJoke').innerHTML = `<p>${JSON.parse(localStorage.getItem("joke"))}</p>` ;
 
+        if (imgClick === 7) imgClick = -1;
+        imgClick ++;
+        const img = ChuckImg[imgClick];
+        console.log("img", img)
+        document.getElementById('imagem_aleatoria').innerHTML = `<img src="img/${img}" alt="Imagem do Chuck Norris">`;
         
         
     })    
